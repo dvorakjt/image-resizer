a96877794ff591b49a88299bb2e332c3c3716cf8
Joseph Dvorak <dvorakjt@gmail.com>
2025-11-18 12:36:28 -0800

    resized IRCheckbox

--- ImageResizer/MainPage.xaml.cs
+++ ImageResizer/MainPage.xaml.cs
@@ -8,9 +8,36 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
         var textInput = new TextInputBuilder().Build();
-        var withLabel = new TextInputBuilder().WithLabel("Hello World").NumericAllowZero().Build();
-        
         MainLayout.Children.Add(textInput);
-        MainLayout.Children.Add(withLabel);
+        
+        var cb1 = new IRCheckbox()
+        {
+            IsChecked = true,
+            LabelText = "Starts Checked and Enabled"
+        };
+        
+        var cb2 = new IRCheckbox()
+        {
+            IsChecked = false,
+            LabelText = "Starts Unchecked and Enabled"
+        };
+
+        var cb3 = new IRCheckbox()
+        {
+            IsEnabled = false,
+            LabelText = "Starts Unchecked and Disabled"
+        };
+
+        var cb4 = new IRCheckbox()
+        {
+            IsChecked = true,
+            IsEnabled = false,
+            LabelText = "Starts Checked and Disabled"
+        };
+        
+        MainLayout.Children.Add(cb1);
+        MainLayout.Children.Add(cb2);
+        MainLayout.Children.Add(cb3);
+        MainLayout.Children.Add(cb4);
     }
 }
