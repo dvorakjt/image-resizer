927cf30f3b7a621fec1149cd1ec80c1d3d950335
Joseph Dvorak <dvorakjt@gmail.com>
2025-11-19 00:41:05 -0800

    style updates

--- ImageResizer/MainPage.xaml.cs
+++ ImageResizer/MainPage.xaml.cs
@@ -9,6 +9,22 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
 
+        var heading = new Label()
+        {
+            Text = "Heading",
+            StyleClass = ["Heading"]
+        };
+        
+        MainLayout.Children.Add(heading);
+
+        var subheading = new Label()
+        {
+            Text = "Subheading1",
+            StyleClass = ["Subheading1"]
+        };
+        
+        MainLayout.Children.Add(subheading);
+
         var responsivenessModesInput = new CustomRadioButtonGroup
         (
             [
@@ -33,5 +49,49 @@ public partial class MainPage : ContentPage
         );
         
         MainLayout.Children.Add(responsivenessModesInput);
+
+        var textInput = new TextInputBuilder().WithLabel("Text Input").WithValidator(FormControlHelpers.CreateRequiredFieldValidator("Required")).Build();
+        MainLayout.Children.Add(textInput);
+        
+        var checkboxGroup =  new CustomCheckboxGroup(
+            [
+                new CheckboxGroupItem
+                {
+                    Value = Density.OneX.ToHtmlString(), 
+                    Label = Density.OneX.ToHtmlString(),
+                    IsChecked = true, 
+                    IsFrozen = true
+                },
+                new CheckboxGroupItem
+                {
+                    Value = Density.OneDot5X.ToHtmlString(), 
+                    Label = Density.OneDot5X.ToHtmlString(),
+                    IsChecked = true, 
+                },
+                new CheckboxGroupItem
+                {
+                    Value = Density.TwoX.ToHtmlString(), 
+                    Label = Density.TwoX.ToHtmlString(),
+                    IsChecked = true, 
+                },
+                new CheckboxGroupItem
+                {
+                    Value = Density.ThreeX.ToHtmlString(), 
+                    Label = Density.ThreeX.ToHtmlString(),
+                    IsChecked = true, 
+                },
+                new CheckboxGroupItem
+                {
+                    Value = Density.FourX.ToHtmlString(), 
+                    Label = Density.FourX.ToHtmlString(),
+                    IsChecked = true, 
+                },
+            ]
+        )
+        {
+            LabelText = "Densities",
+        };
+        
+        MainLayout.Children.Add(checkboxGroup);
     }
 }
