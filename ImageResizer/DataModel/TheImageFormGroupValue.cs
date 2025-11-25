f8b18c16091216135ca22ae13d1a76711141c82a
Joseph Dvorak <dvorakjt@gmail.com>
2025-11-25 14:52:01 -0800

    TheImageFormGroup implements IFormElement

---
+++ ImageResizer/DataModel/TheImageFormGroupValue.cs
@@ -0,0 +1,7 @@
+namespace ImageResizer.DataModel;
+
+public class TheImageFormGroupValue
+{
+    public Stream? ImageStream { get; init; }
+    public string AltText { get; init; }
+}
--- ImageResizer/FormGroups/TheImage/TheImageFormGroup.xaml.cs
+++ ImageResizer/FormGroups/TheImage/TheImageFormGroup.xaml.cs
@@ -1,9 +1,31 @@
+using ImageResizer.DataModel;
 using ImageResizer.FormControls;
 
 namespace ImageResizer.FormGroups.TheImage;
 
-public partial class TheImageFormGroup : ContentView
+public partial class TheImageFormGroup : ContentView, IFormElement<TheImageFormGroupValue>
 {
+    
+    public event EventHandler<IFormElementState<TheImageFormGroupValue>>? StateChanged;
+
+    public IFormElementState<TheImageFormGroupValue> State
+    {
+        get
+        {
+            var isValid = _imagePicker.State.IsValid && _altTextInput.State.IsValid;
+            return new FormElementState<TheImageFormGroupValue>()
+            {
+                Value = new TheImageFormGroupValue()
+                {
+                    ImageStream = _imagePicker.State.Value,
+                    AltText = _altTextInput.State.Value,
+                },
+                IsValid = isValid,
+                ErrorMessage = ""
+            };
+        }
+    }
+
     private ImagePicker _imagePicker;
     private TextInput _altTextInput;
     
@@ -16,6 +38,7 @@ public partial class TheImageFormGroup : ContentView
     private void InitializeFormControls()
     {
         _imagePicker = new ImagePicker();
+        _imagePicker.StateChanged += (sender, e) => StateChanged?.Invoke(this, State);
         RootLayout.Children.Add(_imagePicker);
 
         _altTextInput = new TextInputBuilder()
@@ -25,7 +48,20 @@ public partial class TheImageFormGroup : ContentView
             )
             .Build();
         
+        _altTextInput.StateChanged += (sender, e) => StateChanged?.Invoke(this, State);
         RootLayout.Children.Add(_altTextInput);
+    }
+    
+    public void DisplayErrors()
+    {
+        _imagePicker.DisplayErrors();
+        _altTextInput.DisplayErrors();
+    }
+
+    public void Revalidate()
+    {
+        _imagePicker.Revalidate();
+        _altTextInput.Revalidate();
     }
 
     public void Reset()
